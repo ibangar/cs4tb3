92dfb597f583263227cbae0754185c939bcf8529
arcjustin <arcjustin3@gmail.com>
2015-03-01 15:33:40 -0500

    a2/p2 : added entire compiler source to project.

--- assignment2/part2/analyser.pas
+++
@@ -1,9 +0,0 @@
-program analyser;
-uses scanner;
-begin
-    GetSym;
-    while not (sym = EofSym) do
-    begin
-        GetSym;
-    end;
-end.
--- assignment2/part2/test/test.pas
+++ assignment2/part2/test/test.pas
@@ -1,8 +1,4 @@
 program test;
-var thisidentifieris30characers123;
-var thisidentifieris31characers1234;
-var thisidentifieris32characers12345;
-var smallidentifier;
 begin
-    { { comment } }
+    write('hi');
 end.
