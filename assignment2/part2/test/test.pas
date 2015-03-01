de8cb3fd0dbf5ff2480cc6927cd4ac14af1eeea5
arcjustin <arcjustin3@gmail.com>
2015-03-01 15:37:40 -0500

    a2/p2 : use given Pascal0 program in test.

--- assignment2/part2/test/test.pas
+++ assignment2/part2/test/test.pas
@@ -1,4 +1,16 @@
-program test;
+program arithmetic;
+
+  var x, y, q, r: integer;
+
+  procedure QuotRem (x, y: integer; var q, r: integer);
+  begin q := 0; r := x;
+    while r >= y do { q*y+r=x and r>=y }
+      begin r := r - y; q := q + 1
+      end
+  end;
+
 begin
-    write('hi');
+  read (x); read (y);
+  QuotRem (x, y, q, r);
+  write (q); write (r); writeln
 end.
